8fd093421ee7897cfe3a589978111cd9017c3a28
Marcelo Malacalza <MalacalzaMarcelo@gmail.com>
2021-12-08 12:40:33 -0300

    feat/contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Contact.scss';
 
 export default function Contact() {
+    const [message, setMessage] = useState(false)
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setMessage(true);
+    }
     return (
         <div className='contact' id='contact'>
-            
+            <div className="left">
+                <img src="assets/handshake.png" alt="handshake" />
+            </div>
+            <div className="right">
+                <h2>Contacto</h2>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder='Email' />
+                    <textarea placeholder='Mensaje'></textarea>
+                    <button type='submit'>Enviar</button>
+                    {message && <span>Gracias por contactarte conmigo!</span>}
+                </form>
+            </div>
         </div>
     )
 }
